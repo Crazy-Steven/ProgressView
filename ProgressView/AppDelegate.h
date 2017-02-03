9c501f2846cb0ce520b8f64fabc240eaa106b401
Crazy Steven <956995511@qq.com>
2017-02-03 17:17:00 +0800

    Initial Commit

---
+++ ProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgressView
+//
+//  Created by Steven on 2017/2/3.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressView
+//
+//  Created by Steven on 2017/2/3.
+//  Copyright © 2017年 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
